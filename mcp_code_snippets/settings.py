504ac9d07d6a6e4c3b3420aa53c594f966f83db0
George <justmist15@gmail.com>
2025-06-10 16:55:05 +0400

    fix: do not allow set system root as project root, replace system root with project root if in cursor

--- mcp_code_snippets/settings.py
+++ mcp_code_snippets/settings.py
@@ -1,6 +1,10 @@
+import os
+from pathlib import Path
 from typing import Optional
-from pydantic import Field
+
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings
+
 from fastmcp.utilities.mcp_config import MCPConfig
 
 from mcp_code_snippets.explore.detect_language import ProgrammingLanguage
@@ -38,3 +42,34 @@ class ProxySettings(BaseSettings):
         description="Configuration file with information about remote MCP servers to connect to",
         validation_alias="MCP_PROXY_CONFIG",
     )
+
+    @field_validator("project_root_path", mode="before")
+    @classmethod
+    def validate_project_root_path(cls, v: str) -> str:
+        """Validate the project root path
+
+        PROJECT_ROOT_PATH is assumed to be set manually, so it takes precedence over the other cases.
+        WORKSPACE_FOLDER_PATHS is an environment variable coming from Cursor. By default, Cursor assumes that
+            paths are relative to the system root when it's launching MCP servers. This might be inconvenient, and we
+            replace default value './' with the current working directory.
+
+        Args:
+            v (str): The project root path from the environment variable or default value.
+        """
+        project_root_env = os.environ.get("PROJECT_ROOT_PATH")
+        workspace_folder_paths_env = os.environ.get("WORKSPACE_FOLDER_PATHS")
+
+        if project_root_env:
+            v = project_root_env
+        elif workspace_folder_paths_env:
+            v = workspace_folder_paths_env
+
+        if v == './':
+            abs_path = Path(v).resolve()
+            if abs_path == Path("/"):
+                raise ValueError(
+                    "Project root path cannot resolve to system root '/', "
+                    "please specify a valid path via `PROJECT_ROOT_PATH` environment variable."
+                )
+
+        return v
